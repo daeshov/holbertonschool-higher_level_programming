061352668dc23d18493833b28062f0e6593c5fd0
root <jdarahthomas@gmail.com>
2023-01-11 02:07:32 +0000

    task 1

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     if numarg == 1:
         print('{} argument:'.format(numarg))
     else:
-        print('{} arguments:'.format(numarg))
+        print('{} arguments.'.format(numarg))
     for i in range(1, numarg + 1):
         print('{}: {}'.format(i, sys.argv[i]))
