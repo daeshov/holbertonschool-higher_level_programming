40ab7c540b63e6a37ed6f3285fa8a174a7525f6c
root <jdarahthomas@gmail.com>
2023-01-11 02:15:57 +0000

    task 1

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-import sys
-if __name__ == '__main__':
-    numarg = len(sys.argv) - 1
-    if numarg == 1:
-        print('{} argument:'.format(numarg))
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print('{} arguments:'.format(numarg))
-    for i in range(1, numarg + 1):
-        print('{}: {}.'.format(i, sys.argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
