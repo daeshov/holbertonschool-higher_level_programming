c4940be1d91c4843a5da07a7e91fb94606bbd532
root <jdarahthomas@gmail.com>
2023-01-12 20:50:03 +0000

    task 1

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-     if idx < 0:
+    if idx < 0:
         return None
-     if idx > len(My_list):
+    if idx > len(my_list):
         return None
-     return my_list[idx]
+    return my_list[idx]
+
