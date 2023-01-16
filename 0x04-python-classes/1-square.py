a78a1a1f7cd089a2b3537048b723fed85adb1a79
root <jdarahthomas@gmail.com>
2023-01-16 18:27:42 +0000

    task 1

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -11,10 +11,15 @@ class Square:
     Args:
         size : size of a side in square
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
         Initializes square
         Attributes:
             size: size of a side of square
         """
-        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
