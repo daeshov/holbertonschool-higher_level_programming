091bdf6211afcc7f9f60aac6308018f3011693f7
root <jdarahthomas@gmail.com>
2023-01-20 20:57:46 +0000

    task 1

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -37,11 +37,12 @@ class Rectangle():
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def perimeter(self):
+
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-    
+
     def area(self):
         return self.__width * self.__height
-    
