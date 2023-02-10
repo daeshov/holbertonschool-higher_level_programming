37cf3b89a37ec2f41ec673f926a54fa4f5aa97c3
daeshov <jdarahthomas@gmail.com>
2023-02-10 13:26:18 -0800

    task 01

---
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER 
+    IF NOT EXISTS 'user_0d_1'
+    IDENTIFIED By 'user_0d_1_pwd';
+GRANT ALL privileges
+   ON *.*
+   TO 'user_0d_1';
