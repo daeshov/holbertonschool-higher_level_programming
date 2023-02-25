e6792ef7fd357c06eb538e384fd777ee9ee23c5a
daeshov <jdarahthomas@gmail.com>
2023-02-25 13:15:54 -0800

    task 001

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python3
+
 """
-lists all states with a name starting with N from database hbtn_0e_0_usa
+Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+Usage: ./1-filter_states.py <mysql username> \
+                             <mysql password> \
+                             <database name>
 """
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database)
-
-        cursor = db.cursor()
-
-        cursor.execute
-        ("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-        for state in cursor.fetchall():
-            print(state)
-
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL database: {}".format(e))
-        exit(1)
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY id ASC")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
