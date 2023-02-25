96d4aa2a7babe4cb2df583174bc4c1d550057732
daeshov <jdarahthomas@gmail.com>
2023-02-25 09:54:51 -0800

    task 0

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,34 @@ Usage: ./0-select_states.py <mysql username> \
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.Connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database)
+
+        cursor = db.cursor()
+
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        for state in cursor.fetchall():
+            print(state)
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL database: {}".format(e))
+        exit(1)
