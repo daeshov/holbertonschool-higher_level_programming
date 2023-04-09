933fbdb5916d58f7c5a8b32f1e4d85e93a81a5c6
root <jdarahthomas@gmail.com>
2023-04-09 18:12:20 +0000

    task 0

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
 const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found')
+
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
