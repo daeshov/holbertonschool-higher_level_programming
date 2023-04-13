cf5cf5c2c25a31d32823e3acad84b1987b2ad752
root <jdarahthomas@gmail.com>
2023-04-13 18:50:57 +0000

    task 1

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const file = process.argv[2];
-
+// process = node.js exec (0), pth > scrt(1)..
 fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
