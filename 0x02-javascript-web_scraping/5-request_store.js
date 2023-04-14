3777a33f7fa58b5b65e656e198d40eeaa5c4da00
root <jdarahthomas@gmail.com>
2023-04-14 18:07:15 +0000

    task 5

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const url = process.argv[2];
-const filename = process.argv[3];
+const filepath = process.argv[3];
 const fs = require('fs');
 const request = require('request');
 
@@ -9,6 +9,6 @@ request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filename, body, 'utf-8');
+    fs.writeFile(filepath, body, 'utf-8');
   }
 });
