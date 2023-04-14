97e69729963337f409519b5f56726b22789f0df1
root <jdarahthomas@gmail.com>
2023-04-14 18:05:12 +0000

    task 5

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -9,6 +9,6 @@ request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filename, body, 'utf8');
+    fs.writeFile(filename, body, 'utf-8');
   }
 });
