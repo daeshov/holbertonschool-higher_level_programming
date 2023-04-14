d3f5a77a8970322fa19a70835011e51f82aadad2
root <jdarahthomas@gmail.com>
2023-04-14 18:12:04 +0000

    task 5

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
 
-const url = process.argv[2];
-const filepath = process.argv[3];
 const fs = require('fs');
 const request = require('request');
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filepath, body, 'utf-8');
-  }
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
